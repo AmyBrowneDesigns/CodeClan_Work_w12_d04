4c8b6f8a64d534afe7112bfcd5ccacf2f078b86e
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-11-26 15:59:07 +0000

    started attach method in gamestest

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,36 @@
+import Characters.Enemies.Enemy;
+import Characters.Enemies.EnemyType;
+import Characters.Magical.Familiar;
+import Characters.Magical.Magical;
+import Characters.Magical.MagicalType;
+import Characters.Magical.Spell;
+import Loot.LootContainer;
+import Rooms.Room;
+import Rooms.Type;
+import Rooms.Size;
+import org.junit.Before;
+import org.junit.Test;
+
 public class GameTest {
 
+    Game testGame;
+    Magical wizard;
+    Enemy orc;
+    LootContainer treasureChest;
+    Room room;
+
+    @Before
+    public void before(){
+        wizard = new Magical("Gandalf", 30, 40, 20, 50, MagicalType.WIZARD, Spell.CRUCIO, Familiar.PSYCHOSQUIRREL);
+        orc = new Enemy("jericho", 20, 10, 30, 20, 80, EnemyType.ORC,);
+        treasureChest = new LootContainer("Rubies", 200, 10);
+        room = new Room(Size.SMALL, Type.CAVE, orc,treasureChest);
+    }
+
+    @Test
+    public void wizardCanAttackOrc(){
+        wizard.attack()
+    }
 
 
 }
